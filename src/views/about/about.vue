ed6302673d17c781c2b8a731a133892f0ec6efd3
pengjianrong <brdkurin.peng@jodoinc.com>
2024-11-14 18:51:43 +0800

    update: 完善博客

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -1,6 +1,127 @@
 <template>
-  <div>
-    
+  <div class="kur_detail_about">
+    <h1 class="kur_detail__title">关于</h1>
+    <div class="kur_detail__count">
+      <div class="kur_detail__count-information">
+        <img width="35" height="35" class="avatar ls-is-cached lazyloaded" src="" alt="GabrielxD">
+        <div class="meta">
+          <div class="author">
+            <a class="link" href="https://gabrielxd.com/s/about" title="GabrielxD">GabrielxD</a>
+          </div>
+          <div class="item">
+            <span class="text">2024-06-07</span>
+          </div>
+        </div>
+      </div>
+      <time class="kur_detail__count-created" datetime="06/13">06/13</time>
+    </div>
+    <article class="kur_detail__article animated fadeIn left-img line-numbers">
+      <h3 id="%E5%85%B3%E4%BA%8E%E6%88%91" tabindex="-1">关于我</h3>
+      <h4 id="%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF" tabindex="-1">基本信息</h4>
+      <p>
+        <code>GabrielxD</code>
+        <code>男</code>
+        <code>20岁</code>
+        <code>大学生</code>
+        <code>宅宅</code>
+        <code>ISTJ-A</code>
+      </p>
+      <h4 id="%E5%96%9C%E6%AC%A2%E7%9A%84%E4%B8%9C%E8%A5%BF" tabindex="-1">喜欢的东西</h4>
+      <p>
+        <code>Anime</code>
+        <code>Vtuber</code>
+        <code>Coding</code>
+        <code>Galgame</code>
+      </p>
+      <h4 id="%E4%BA%86%E8%A7%A3%E7%9A%84%E8%AF%AD%E8%A8%80" tabindex="-1">了解的语言</h4>
+      <kur-raw-content style="--prog-logo-width: 6.5em; --prog-logo-ratio: 1.5; margin-bottom: 18px; display: inline-block;">
+        <div
+          style="display: inline-block;
+          width: var(--prog-logo-width);
+          height: calc(var(--prog-logo-width) / var(--prog-logo-ratio));
+          background-image: url('https://img.gabrielxd.top/blog/Golang.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;" aria-label="Golang"
+        ></div>
+        <div
+          style="display: inline-block;
+          width: var(--prog-logo-width);
+          height: calc(var(--prog-logo-width) / var(--prog-logo-ratio));
+          background-image: url('https://img.gabrielxd.top/blog/C.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;" aria-label="C"
+        ></div>
+        <div
+          style="display: inline-block;
+          width: var(--prog-logo-width);
+          height: calc(var(--prog-logo-width) / var(--prog-logo-ratio));
+          background-image: url('https://img.gabrielxd.top/blog/C++.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;" aria-label="C++"
+        ></div>
+        <div
+          style="display: inline-block;
+          width: var(--prog-logo-width);
+          height: calc(var(--prog-logo-width) / var(--prog-logo-ratio));
+          background-image: url('https://img.gabrielxd.top/blog/Java.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;" aria-label="Java"
+        ></div>
+        <div
+          style="display: inline-block;
+          width: var(--prog-logo-width);
+          height: calc(var(--prog-logo-width) / var(--prog-logo-ratio));
+          background-image: url('https://img.gabrielxd.top/blog/Node.js.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;" aria-label="Node.js"
+        ></div>
+      </kur-raw-content>
+      <h4 id="%E5%B8%B8%E7%94%A8%E7%9A%84%E7%BC%96%E8%BE%91%E5%99%A8" tabindex="-1">常用的编辑器</h4>
+      <p>
+        <code>Notepad3</code>
+        <code>Visual Studio Code</code>
+        <code>Sublime Text</code>
+        <code>Typora</code>
+        <code>GoLand</code>
+      </p>
+      <br>
+      <h3 id="%E5%85%B3%E4%BA%8E%E6%9C%AC%E7%AB%99" tabindex="-1">关于本站</h3>
+      <p>
+        <kur-message type="warning" content="本站 LOGO 及博主头像均为私人约稿，未经允许请勿擅自使用。">
+          <span class="kur_message warning">
+            <span class="kur_message__icon"></span>
+            <span class="kur_message__content">本站 LOGO 及博主头像均为私人约稿，未经允许请勿擅自使用。</span>
+          </span>
+        </kur-message>
+      </p>
+      <kur-raw-content>
+        <div class="kur_message info" style="margin-bottom: 18px">
+          <span class="kur_message__icon"></span>
+          <div class="kur_message__content">
+            <em style="font-style:normal; font-weight:bold;">上文中所用到的
+              <a href="#%E4%BA%86%E8%A7%A3%E7%9A%84%E8%AF%AD%E8%A8%80" rel="noopener noreferrer nofollow" target="">
+                编程语言图标
+              </a>
+              版权信息如下：
+            </em>
+            <br>
+            版权所属：
+            <a href="https://x.com/sawaratsuki1004" target="_blank" rel="noopener noreferrer nofollow">@Sawaratsuki</a>
+            <br>
+            项目链接：
+            <a href="https://github.com/SAWARATSUKI/KawaiiLogos" target="_blank" rel="noopener noreferrer nofollow">https://github.com/SAWARATSUKI/KawaiiLogos</a>
+            <br>
+            许可协议：
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans" target="_blank" rel="noopener noreferrer nofollow">署名-非商业性使用-相同方式共享 4.0 国际 (CC BY-NC-SA 4.0)</a>
+          </div>
+        </div>
+      </kur-raw-content>
+    </article>
   </div>
 </template>
 
@@ -8,6 +129,172 @@
 
 </script>
 
-<style lang="scss">
-  
+<style lang="scss" scoped>
+  .kur_detail {
+    &__title {
+      font-weight: 500;
+      font-size: px2rem(24px);
+      color: var(--classF);
+      text-align: center;
+      padding-top: px2rem(40px);
+      margin-bottom: px2rem(15px);
+      word-break: break-word;
+      text-shadow: var(--text-shadow);
+    }
+    &__count {
+      position: relative;
+      -webkit-box-pack: justify;
+      -ms-flex-pack: justify;
+      justify-content: space-between;
+      padding-bottom: px2rem(15px);
+      margin-bottom: px2rem(10px);
+      border-bottom: 1px solid var(--classC);
+      @include display-flex-center;
+      &-information {
+        @include display-flex-center;
+        .avatar {
+          width: px2rem(35px);
+          height: px2rem(35px);
+          border-radius: 50%;
+          margin-right: px2rem(10px);
+          padding: px2rem(3px);
+          -o-object-fit: cover;
+          object-fit: cover;
+          background: var(--classC);
+          border: 1px solid var(--classA);
+        }
+        .meta {
+          -webkit-box-orient: vertical;
+          -webkit-box-direction: normal;
+          -ms-flex-direction: column;
+          flex-direction: column;
+          height: px2rem(35px);
+          -webkit-box-pack: justify;
+          -ms-flex-pack: justify;
+          justify-content: space-between;
+          font-size: px2rem(12px);
+          @include display-flex;
+          .author {
+            .link {
+              font-weight: 500;
+              color: var(--theme);
+            }
+          }
+          .item {
+            color: var(--minor);
+            line-height: px2rem(16px);
+            .line {
+              color: var(--seat);
+              margin: 0 px2rem(5px);
+              vertical-align: middle;
+            }
+          }
+        }
+      }
+      .kur_detail__count {
+        font-size: px2rem(32px);
+        line-height: px2rem(42px);
+        color: var(--routine);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        text-shadow: var(--text-shadow);
+      }
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: px2rem(-1.5px);
+        left: 0;
+        width: px2rem(80px);
+        height: px2rem(3px);
+        border-radius: px2rem(1.5px);
+        background: var(--theme);
+      }
+    }
+    &__article {
+      padding: px2rem(10px) 0;
+      font-size: px2rem(15px);
+      word-break: break-word;
+      color: var(--routine);
+      h3 {
+        margin: px2rem(30px) 0 px2rem(18px);
+        font-size: px2rem(20px);
+        &::before {
+          left: 0;
+          width: px2rem(20px);
+          height: 20px;
+          background-size: auto 100%;
+          position: relative;
+          display: inline-block;
+          vertical-align: middle;
+          content: "";
+          top: -3px;
+          margin-right: 8px;
+          background-position: center;
+          background-repeat: none;
+          background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAH1JREFUWEft1qENgDAQheH/JANg60DAJIgmrMFCzEEYgTFgDyRFIClp0yBf9evl8pl3RsYbp7BjNLHoMptljPiMZH3WAhKQgAQkIIGXQAXUT79cniHaNMa5draliqqojIID86nRHEtvbSqlBSQgAQlIQAISkECRAA746SC5Ad6XpiGnnOGPAAAAAElFTkSuQmCC);
+        }
+      }
+      h4 {
+        font-size: px2rem(18px);
+        color: var(--main);
+        line-height: px2rem(24px);
+        margin-bottom: px2rem(18px);
+        position: relative;
+        &::before {
+          position: relative;
+          display: inline-block;
+          vertical-align: middle;
+          content: "";
+          margin-right: px2rem(6px);
+          background-position: center;
+          background-repeat: none;
+          top: px2rem(-2px);
+          width: px2rem(22px);
+          height: px2rem(22px);
+          color: var(--theme);
+          background-size: auto 100%;
+          background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAndJREFUWEftVkty2kAQfT1SBLtwg6AqwzbxCQIniG8QvAmwCp8kRqvgFa5KwHjlkI3NCeIbhJwgXptUgW9AdiDQdGoITqUsjcZyuYqNZ6WSXs88vel+3YQtL9ry+Xgk8KhApALNAe8x5PPbCUrgabdsD2/e+1edEpN4FsIRps7OwT9cXKKHCLw75QIL+V0XRFIUP1dptBx3ChKkxQlw8UnOG5mqLESgdsYZy5dTAE8jg5lH3Ypd5Mlxxl8ttDgGRulcq5iYgApofgn6ILzVBQsSu5/e0OXiqnMOotdatVjsOvkPl4muQIFrp5y1hJzEBA67ZavEk07WX5EexzxM5b1SYgJrFQbBOQDt3wVSuP0qTRfjowsAr3SHODa75HrqqiKX1gdMycjAYa9stU3JCPBhKue1ExNQAY3BakSgl1HBDMykI9z+Ps0WV0eXIITKdhM3c+yUS259FrVPrBM2TlclEnSmY8+S93tV+/yvH+hxxLzv5D11paFltOLmIFD3FzIbtRMzpr2K5arn+bgzJZAWl8631rjby0zAUJI3xnTfkjQTiFEAwHW3bGWNCoCv0zlvjUukgOoJgPymzQGg3itbfVMOMKiezh30ExOIqwIAvwNHZFUVzMcdbbUonGOnsomr4P1XfiFZ/tS6GOOkW7FqJh8gwomz06ol9oE7O6GhH9zLCbfeCxqDoE3AR61sm5nA/3XUZ47pmneYCSLnAeHLCQGZaAvmH72yXdjMA6oTanHpnFfQ5tDmQ+KJ6MZ+jckXY7//k4pWYB7sQVCUccxU3a9teHKcWS7ne0wI4Rhipqv7REZkku8hvhut+CEOidvjkcDWFfgD9RMzMKE7f80AAAAASUVORK5CYII=);
+        }
+      }
+      p {
+        line-height: px2rem(26px);
+        margin-bottom: px2rem(18px);
+        code:not([class]) {
+          display: inline;
+          padding: px2rem(3px) px2rem(8px);
+          margin: 0;
+          white-space: nowrap;
+          display: inline-block;
+          font-size: px2rem(12px);
+          color: var(--code);
+          margin: px2rem(2px) px2rem(5px);
+          padding: 0 px2rem(8px);
+          white-space: normal;
+          text-indent: 0;
+          -webkit-user-select: auto;
+          -moz-user-select: auto;
+          -ms-user-select: auto;
+          user-select: auto;
+          vertical-align: baseline;
+          word-break: break-word;
+          background: var(--code-background);
+          border-radius: var(--radius-inner);
+        }
+      }
+      .kur_message {
+        display: block;
+        position: relative;
+        border-left-width: px2rem(4px);
+        border-left-style: solid;
+        padding: px2rem(8px) px2rem(15px);
+        border-radius: 0 px2rem(4px) px2rem(4px) 0;
+        line-height: px2rem(26px);
+        border-left-color: #1d72f3;
+        background: #1d72f320;
+        color: #1d72f3;
+      }
+    }
+  }
 </style>
